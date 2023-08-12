71ec1bcf74b6c193692628bb64617a1083a85ec4
Oleksandr Moroz <felplenz1998@gmail.com>
2023-08-12 14:01:42 +0200

    HL-3: My version for ArrayList

---
+++ src/com/hillel/course2/lesson1/homework/MyArrayList.java
@@ -0,0 +1,76 @@
+package com.hillel.course2.lesson1.homework;
+
+public class MyArrayList implements MyArrayListInterface {
+
+    private String[] arrays;
+
+    public MyArrayList() {
+        this.arrays = new String[]{};
+    }
+
+    @Override
+    public String get(int index) {
+        return this.arrays[index];
+    }
+
+    @Override
+    public void set(int index, String string) {
+        if (this.arrays.length <= index) {
+            this.arrays = copyOf(this.arrays, index);
+        }
+        this.arrays[index] = string;
+    }
+
+    @Override
+    public void remove(int index) {
+        this.arrays[index] = null;
+        String[] string = new String[this.arrays.length - 1];
+        for (int i = 0; i < this.arrays.length - 1; i++) {
+            if (i >= index) {
+                string[i] = this.arrays[i + 1];
+                continue;
+            }
+            string[i] = this.arrays[i];
+        }
+        this.arrays = string;
+    }
+
+    @Override
+    public void add(int index, String string) {
+        if (this.arrays.length - 1 < index) {
+            this.arrays = copyOf(this.arrays, index);
+            this.arrays[index] = string;
+            return;
+        }
+
+        this.arrays = copyOf(this.arrays, this.arrays.length);
+        for (int i = this.arrays.length - 1; i >= index; i--) {
+            this.arrays[i] = this.arrays[i - 1];
+        }
+        this.arrays[index] = string;
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i <= this.arrays.length - 1; i++) {
+            if (i == 0) {
+                result += "[";
+                continue;
+            } else if (i == this.arrays.length - 1) {
+                result += this.arrays[i] + "]";
+                continue;
+            }
+            result += this.arrays[i] + ", ";
+        }
+        return result;
+    }
+
+    private String[] copyOf(String[] array, int index) {
+        String[] string = new String[index + 1];
+        for (int i = 0; i < array.length; i++) {
+            string[i] = array[i];
+        }
+        return string;
+    }
+}
---
+++ src/com/hillel/course2/lesson1/homework/MyArrayListInterface.java
@@ -0,0 +1,9 @@
+package com.hillel.course2.lesson1.homework;
+
+public interface MyArrayListInterface {
+    String get(int index);
+    void set(int index, String string);
+    void remove(int index);
+    void add(int index, String string);
+    String toString();
+}
